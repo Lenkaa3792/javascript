130c529611bea6fee625cb584373378410d6d378
Lenkaa3792 <allanlenkaa@gmail.com>
2022-05-26 23:44:34 +0300

    adds js basics

---
+++ index.js
@@ -0,0 +1,21 @@
+//alert( "This is an inline js" );
+//printing on the webpage 
+document.write( '<h1>This is my javascript </h1>' )
+//logging to console
+document.write( "This is my page" +'<br>')
+
+//data types
+//1 numbers
+num1 = 2;
+num2 = 4;
+document.write(num1 + "<br>");
+//adding
+document.write(num1 + num2 + "<br>");
+//strings
+var name = 'john';
+document.write( name + "<br>" );
+//concatenation
+var age = 23;
+var city = 'Nairobi';
+var car = 'lexus';
+document.write('My name is ' + name +'.'+'I live in '+ city + ' and i drive a ' + car <'br>')
